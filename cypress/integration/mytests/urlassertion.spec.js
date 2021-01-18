36f08db8d6dc0b1e97ee32db2c05e1550a6e30fa
Nityanand Gupta <nityanandgupta@Nityanands-MacBook-Pro.local>
2021-01-18 09:46:12 +0100

    urlassertion updated

--- cypress/integration/mytests/urlassertion.spec.js
+++ cypress/integration/mytests/urlassertion.spec.js
@@ -3,9 +3,8 @@ describe('launch my app', ()=> {
     it('app testing', ()=>{
 
         cy.visit('https://www.spicejet.com')
-        cy.contains('Covid-19 Information')
-        cy.contains('Covid-19 Information').click()
-        cy.url().should('include', '/covid19.aspx') //This is an assertation
+        cy.contains('DEALS')
+        cy.contains('DEALS').click()
+        cy.url().should('include', '/RedHotOffers') //This is an assertation
     })
-
 })
